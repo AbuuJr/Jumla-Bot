ceed99c554ec52e5631b126191d9e87ad098b18d
JackronyK <karani.riungu.a@gmail.com>
2026-01-16 07:41:22 +0300

    Updated config files

--- jumla-frontend/vite.config.ts
+++ jumla-frontend/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.cjs'
+    postcss: './postcss.config.cjs',
   },
   resolve: {
     alias: {
